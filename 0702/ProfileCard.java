7783d55b25a92274719e350adf6cd1d3986deafb
Yellow0124 <412631250@o365.tku.edu.tw>
2026-07-02 10:48:30 +0800

    0702

---
+++ 0702/ProfileCard.java
@@ -0,0 +1,13 @@
+public class ProfileCard {
+    public static void main(String[] args) {
+        String name = "Amy"; 
+        String Department = "Information Management";
+        String FavoriteCourse = "Java Programming";
+
+        System.out.println("=== Profile ===");
+        System.out.println("Name: " + name); //輸出姓名
+        System.out.println("Department: " + Department); //輸出系所
+        System.out.println("Favorite Course: " + FavoriteCourse); //輸出最愛課程
+
+    }
+}
---
+++ 0702/UserInfo.java
@@ -0,0 +1,11 @@
+public class UserInfo {
+    public static void main(String[] args) {
+        String userName = "Amy";
+        int age = 20;
+        double height = 1.65;
+
+        System.out.println("Name: " + userName);
+        System.out.println("Age: " + age);
+        System.out.println("Height: " + height);
+    }
+}
